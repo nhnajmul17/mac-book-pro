76639af72f73998ed1f8ffb7cb512b5d6fe9c575
nhnajmul17 <nhnajmul17@gmail.com>
2021-08-21 11:07:59 +0600

    function created

--- js/script.js
+++ js/script.js
@@ -1,4 +1,50 @@
+//memory calculate funtion
+
+function extraMemory(cost) {
+    const memoryCost = document.getElementById('memory-cost');
+    memoryCost.innerText = parseInt(cost);
+}
+
+// storage calculate function
+
+function extraStorage(cost) {
+    const storageCost = document.getElementById('storage-cost');
+    storageCost.innerText = parseInt(cost);
+}
+
+// delivery calculate function
+function deliveryFee(cost) {
+    const deliveryCost = document.getElementById('delivery-cost');
+    deliveryCost.innerText = parseInt(cost);
+}
+
+
+// memory calculation
 document.getElementById('memory-8gb').addEventListener('click', function () {
-    const cost = document.getElementById('memory-cost').innerText;
-    console.log(cost);
+    extraMemory(0);
+})
+document.getElementById('memory-16gb').addEventListener('click', function () {
+    extraMemory(180);
+})
+
+
+// storage calculation
+document.getElementById('storage-256gb').addEventListener('click', function () {
+    extraStorage(0);
+})
+document.getElementById('storage-512gb').addEventListener('click', function () {
+    extraStorage(100);
+})
+document.getElementById('storage-1tb').addEventListener('click', function () {
+    extraStorage(180);
+})
+
+
+
+// delivery option
+document.getElementById('free-delivery').addEventListener('click', function () {
+    deliveryFee(0);
+})
+document.getElementById('paid-delivery').addEventListener('click', function () {
+    deliveryFee(20);
 })
