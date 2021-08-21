11de10f1c97ddc7228214666688d7cd0f1f4445f
nhnajmul17 <nhnajmul17@gmail.com>
2021-08-21 12:06:01 +0600

    total price

--- js/script.js
+++ js/script.js
@@ -2,22 +2,46 @@
 
 function extraMemory(cost) {
     const memoryCost = document.getElementById('memory-cost');
-    memoryCost.innerText = parseInt(cost);
+    memoryCost.innerText = cost;
+    getTotal()
 }
 
 // storage calculate function
 
 function extraStorage(cost) {
     const storageCost = document.getElementById('storage-cost');
-    storageCost.innerText = parseInt(cost);
+    storageCost.innerText = cost;
+
+    getTotal()
 }
 
 // delivery calculate function
 function deliveryFee(cost) {
     const deliveryCost = document.getElementById('delivery-cost');
-    deliveryCost.innerText = parseInt(cost);
+    deliveryCost.innerText = cost;
+    getTotal()
+
 }
 
+//calucalte Total price
+
+function getprice(type) {
+    const types = document.getElementById(type + '-cost');
+    const typesPrice = parseInt(types.innerText);
+    return typesPrice;
+}
+
+function getTotal() {
+    const best = getprice('best')
+    const storage = getprice('storage');
+    const memory = getprice('memory');
+    const delivery = getprice('delivery')
+
+    const subtotal = parseInt(best) + parseInt(storage) + parseInt(memory) + parseInt(delivery);
+    document.getElementById('total-price').innerText = subtotal;
+    document.getElementById('total').innerText = subtotal;
+
+}
 
 // memory calculation
 document.getElementById('memory-8gb').addEventListener('click', function () {
@@ -48,3 +72,5 @@ document.getElementById('free-delivery').addEventListener('click', function () {
 document.getElementById('paid-delivery').addEventListener('click', function () {
     deliveryFee(20);
 })
+
+
